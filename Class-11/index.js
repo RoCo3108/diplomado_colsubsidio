7221e39336fef85c54bd35285e1ad67b076c79d8
123456789RoCo <roco272012@hotmail.com>
2022-09-28 01:19:36 -0500

    archivoJS

--- Class-11/index.js
+++ Class-11/index.js
@@ -1,3 +1,5 @@
+
+
 console.log(2+8)
 console.log(2*8)
 console.log(2-8)
---
+++ Class-11/test.js
@@ -0,0 +1,24 @@
+
+let elemento1 = {
+
+    "numero":5,
+    "Nombre": "Ronald "
+
+}
+
+let elemento2 = {
+
+    "numero2":25,
+    "Apellido": " Cortes"
+
+}
+
+const mi_nombre= "Ronald"
+for (const letter in mi_nombre){
+    console.log(mi_nombre[letter])
+}
+
+
+
+/*console.log(elemento1.numero + elemento2.numero2)
+console.log(elemento1.Nombre + elemento2.Apellido)*/
