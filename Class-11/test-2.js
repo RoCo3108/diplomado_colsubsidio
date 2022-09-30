080f17189d9a6360d3ef9992c4774428e18da59f
123456789RoCo <roco272012@hotmail.com>
2022-09-29 22:48:32 -0500

    parametros con JS

--- Class-11/test-2.js
+++ Class-11/test-2.js
@@ -1,9 +1,11 @@
 
-function test(argument){
+function test(argument,argument2){
 
-    if (argument === "RonaldC") {
+        console.log(argument+ " is my name y my last name is " + argument2)
+
+   /* if (argument === "RonaldC") {
         console.log(argument)
     } else {
         console.log("No argumen")
-    }
+    }*/
 }
