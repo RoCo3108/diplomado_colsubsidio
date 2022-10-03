c439918c0c68c2746c64ca394d5c48ead30e7e6e
123456789RoCo <roco272012@hotmail.com>
2022-10-03 15:28:50 -0500

    Agregar-texto-JS

---
+++ Proyecto-JS/insertar-textoJS.js
@@ -0,0 +1,23 @@
+
+function texto(){
+
+    const testA = document.getElementById("texto-addidas")
+    testA.innerHTML="Adidas es una de las marcas más reconocidas en el mercado deportivo, es la favorita por muchos atletas y produce además de calzado deportivo."
+
+    const testn = document.getElementById("texto-nike")
+    testn.innerHTML="empresa multinacional estadounidense dedicada al diseño, desarrollo, fabricación y comercialización de equipamiento deportivo: balones, calzado, ropa."
+
+    const testc = document.getElementById("texto-converse")
+    testc.innerHTML="Converse es una compañía estadounidense de ropa y calzado fundada durante la decada de 1900. Desde 2003 es subsidiaria de Nike"
+
+    const testr = document.getElementById("texto-reebok")
+    testr.innerHTML="Reebok es una empresa estadounidense de zapatillas, ropa, y accesorios deportivos,subsidiaria del grupo Authentic Brands Group."
+
+    const testp = document.getElementById("texto-puma")
+    testp.innerHTML="Puma SE es una empresa alemana fabricante de accesorios, ropa y calzado deportivo,cuya sede principal está en Herzogenaurach, Alemania."
+
+    const testb = document.getElementById("texto-balenciaga")
+    testb.innerHTML="Balenciaga es una casa de moda de lujo española fundada en 1917 por el diseñador Cristóbal Balenciaga en San Sebastián, España"
+
+
+}
