442d3030b4d9cbe21a8e998685df5a4b0b52e6f6
123456789RoCo <roco272012@hotmail.com>
2022-10-03 15:57:46 -0500

    ocultar-mostrar-tabla con js

--- Proyecto-JS/insertar-textoJS.js
+++ Proyecto-JS/insertar-textoJS.js
@@ -19,5 +19,24 @@ function texto(){
     const testb = document.getElementById("texto-balenciaga")
     testb.innerHTML="Balenciaga es una casa de moda de lujo española fundada en 1917 por el diseñador Cristóbal Balenciaga en San Sebastián, España"
 
+    const testNew = document.getElementById("texto-new-balance")
+    testNew.innerHTML="Más conocida simplemente como New Balance (NB), es un fabricante estadounidense de piezas de arco con sede en Boston (Estados Unidos). La fundación data del 1906 con el nombre New Balance Arch Support Company"
+
+    const testNike = document.getElementById("texto-nike2")
+    testNike.innerHTML="empresa multinacional estadounidense dedicada al diseño, desarrollo, fabricación y comercialización de equipamiento deportivo: balones, calzado, ropa, equipo, accesorios y otros artículos deportivos"
+
+    const testaddidas = document.getElementById("texto-addidas2")
+    testaddidas.innerHTML="Adidas es una de las marcas más reconocidas en el mercado deportivo, es la favorita por muchos atletas y produce además de calzado deportivo, otros artículos tipo sport"
+    
+}
+
+function tabla() {
+
+var x = document.getElementById("mytable");
+if (x.style.display === "block") {
+x.style.display = "none";
+} else {
+x.style.display = "block";
+}
 
 }
